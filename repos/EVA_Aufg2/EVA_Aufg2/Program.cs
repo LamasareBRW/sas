da2f02e08800e045dab075b271500469b5ba2fb9
LamasareBRW <98410096+LamasareBRW@users.noreply.github.com>
2025-09-18 08:40:04 +0200

    Add files via upload

---
+++ repos/EVA_Aufg2/EVA_Aufg2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Deklaration
+double lohn;
+double stunden;
+double endLohn;
+double prozent;
+
+// Eingabe
+Console.Write("\n\tGeben Sie den Stundenlohn des Mitarbeiters ein:\t\t\t");
+    lohn = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("\tGeben Sie die gearbeiteten Stunden ihres Mitarbeiters ein:\t");
+    stunden = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("\tGeben Sie den Prozentsatz der BA ein:\t\t\t\t");
+    prozent = Convert.ToDouble(Console.ReadLine());
+
+// Verarbeitung
+prozent = prozent * 0.01;
+endLohn = lohn * stunden * prozent;
+
+// Ausgabe
+Console.WriteLine($"\n\tDer endgültige Lohn beträgt {endLohn}");
