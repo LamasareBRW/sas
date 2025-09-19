c2b4431076b83f455ff32117f81978f679ca940d
LamasareBRW <98410096+LamasareBRW@users.noreply.github.com>
2025-09-19 16:17:37 +0200

    Add files via upload

---
+++ repos/zylinderBerechnen/zylinderBerechnen/Program.cs
@@ -0,0 +1,40 @@
+﻿// nhh
+// Deklaration
+double hoehe;
+double durchmesser;
+double volumen;
+double kreisFlaeche;
+bool fehler;
+
+// Initialisierung
+fehler = false;
+volumen = 69;
+kreisFlaeche = 67;
+
+// Eingabe
+Console.WriteLine("\n\tZylinder Berechnung");
+Console.WriteLine("\t===================");
+
+Console.Write("\n\tGeben Sie die Höhe des Zylinders ein:\t");
+    hoehe = Convert.ToDouble(Console.ReadLine());
+Console.Write("\tGeben Sie den Durchmesser des Zylinders ein:\t");
+    durchmesser = Convert.ToDouble(Console.ReadLine());
+
+if(hoehe <= 0) {
+    fehler = true;
+} if(durchmesser <= 0) {
+    fehler = true;
+}
+
+// Verarbeitung
+if(!fehler) {
+    kreisFlaeche = double.Pi * ((durchmesser / 2) *durchmesser / 2);
+    volumen = kreisFlaeche * hoehe;
+}
+
+// Ausgabe
+if(!fehler) {
+    Console.WriteLine($"\n\tDas Volumen des Zylinders beträgt {volumen} und die Kreisfläche beträgt {kreisFlaeche}.");
+} else {
+    Console.WriteLine("\n\tFehler bei der Eingabe.");
+}
