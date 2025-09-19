4f97a1028864a17cc648197dca638513e23e89bd
LamasareBRW <98410096+LamasareBRW@users.noreply.github.com>
2025-09-19 09:27:08 +0200

    Add files via upload

---
+++ repos/ifBedingung/ifBedingung/Program.cs
@@ -0,0 +1,29 @@
+﻿// Deklaration
+int zahl;
+int ausgabe;
+bool falsch;
+
+// Initialisierung
+falsch = false;
+ausgabe = 69;
+
+// Eingabe
+Console.WriteLine("\n\tBeispielprogramm für if");
+Console.WriteLine("\t=======================");
+Console.Write("\n\tBitte geben Sie eine Zahl ein:\t");
+zahl = Convert.ToInt32(Console.ReadLine());
+if(zahl <= 10) {
+    falsch = true;
+}
+
+// Verarbeitung
+if(!falsch) {           // entspricht falsch == false, dann
+    ausgabe = zahl + 2;
+}
+
+// Ausgabe
+if (!falsch) {
+    Console.WriteLine($"\n\tDas Ergebnis lautet {ausgabe}.");
+} else {
+    Console.WriteLine("\n\tFalsche Eingabe.");
+}
