356ed8ed3f2e5b8c3676b132e1520f2075b0e740
LamasareBRW <98410096+LamasareBRW@users.noreply.github.com>
2025-09-26 08:55:26 +0200

    Add files via upload

---
+++ repos/wurzelEinerZahl/wurzelEinerZahl/Program.cs
@@ -0,0 +1,33 @@
+﻿// Deklaration
+double zahl;
+double loesung;
+bool fehler;
+
+// Initialisierung
+zahl = 2813;
+loesung = 2813;
+fehler = false;
+
+// Eingabe
+Console.WriteLine("\n\tWurzel berechnen");
+Console.WriteLine("\t================");
+
+Console.Write("\n\tGeben Sie zu berechnende Zahl ein:\t");
+    zahl = Convert.ToDouble(Console.ReadLine());
+
+if (zahl <= 0) {
+    fehler = true;
+}
+
+// Verarbeitung
+if (!fehler) {
+    loesung = Math.Sqrt(zahl);
+    loesung = Math.Round(loesung, 3, MidpointRounding.AwayFromZero);    // (Zahl, Nachkommastellen, Rundungsart)
+}
+
+// Ausgabe
+if (!fehler) {
+    Console.WriteLine($"\n\tDas Ergebnis lautet {loesung}");
+} else {
+    Console.WriteLine("\n\tFehler bei der Eingabe: Nur positive Zahlen können berechnet werden.");
+}
