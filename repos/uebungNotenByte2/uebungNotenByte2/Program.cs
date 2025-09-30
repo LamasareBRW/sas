6ff19747100badf63858a2ae59ed8f1904b7c8d8
LamasareBRW <98410096+LamasareBRW@users.noreply.github.com>
2025-09-30 15:14:30 +0200

    Add files via upload

---
+++ repos/uebungNotenByte2/uebungNotenByte2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Deklaration
+byte note;
+
+// Eingabe
+Console.Write("\n\tGeben die Note ein:\t");
+note = Convert.ToByte(Console.ReadLine());
+
+// Ausgabe
+switch (note)
+{
+    case 1:
+        Console.WriteLine("\n\tDie Definition lautet Sehr Gut");
+        break;
+    case 2:
+        Console.WriteLine("\n\tDie Definition lautet Gut");
+        break;
+    case 3:
+        Console.WriteLine("\n\tDie Definition lautet Befriedigend");
+        break;
+    case 4:
+        Console.WriteLine("\n\tDie Definition lautet Ausreichend");
+        break;
+    case 5:
+        Console.WriteLine("\n\tDie Definition lautet Mangelhaft");
+        break;
+    case 6:
+        Console.WriteLine("\n\tDie Definition lautet Ungenügend");
+        break;
+    default:
+        Console.WriteLine("\n\tFalsche Eingabe");
+        break;
+}
