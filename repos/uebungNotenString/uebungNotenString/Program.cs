b76ea096717a29c3c3b66defbac80b0cd321d22d
LamasareBRW <98410096+LamasareBRW@users.noreply.github.com>
2025-09-30 15:20:59 +0200

    Add files via upload

---
+++ repos/uebungNotenString/uebungNotenString/Program.cs
@@ -0,0 +1,31 @@
+﻿// Deklaration
+string note;
+
+// Eingabe
+Console.Write("\n\tGeben die Note ein:\t");
+
+// Ausgabe
+switch (note)
+{
+    case "1":
+        Console.WriteLine("\n\tDie Definition lautet Sehr Gut");
+        break;
+    case "2":
+        Console.WriteLine("\n\tDie Definition lautet Gut");
+        break;
+    case "3":
+        Console.WriteLine("\n\tDie Definition lautet Befriedigend");
+        break;
+    case "4":
+        Console.WriteLine("\n\tDie Definition lautet Ausreichend");
+        break;
+    case "5":
+        Console.WriteLine("\n\tDie Definition lautet Mangelhaft");
+        break;
+    case "6":
+        Console.WriteLine("\n\tDie Definition lautet Ungenügend");
+        break;
+    default:
+        Console.WriteLine("\n\tFalsche Eingabe");
+        break;
+}
