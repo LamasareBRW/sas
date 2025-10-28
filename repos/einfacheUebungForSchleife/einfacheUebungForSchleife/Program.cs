c3d333cc85dd14308e83cde8a63ad75d3ad8a77e
LamasareBRW <98410096+LamasareBRW@users.noreply.github.com>
2025-10-28 10:18:57 +0100

    Add files via upload

---
+++ repos/einfacheUebungForSchleife/einfacheUebungForSchleife/Program.cs
@@ -0,0 +1,26 @@
+﻿// Deklaration
+int startwert;
+int endwert;
+int schrittweite;
+
+// Initialisierung
+
+
+// Eingabe
+Console.WriteLine("\n\tProgramm");
+Console.WriteLine("\t========");
+
+Console.Write("\n\tGeben Sie den Startwert ein:\t");
+    startwert = Convert.ToInt32(Console.ReadLine());
+Console.Write("\tGeben Sie den Endwert ein:\t");
+    endwert = Convert.ToInt32(Console.ReadLine());
+Console.Write("\tGeben Sie die Schrittweite ein:\t");
+    schrittweite = Convert.ToInt32(Console.ReadLine());
+
+// Verarbeitung
+
+
+// Ausgabe
+for (int i = startwert; i <= endwert; i = i + schrittweite) {
+    Console.WriteLine(i);
+}
