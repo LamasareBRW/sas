ac2e2c89826fdd9b87316076eb87fc316d9e4316
LamasareBRW <98410096+LamasareBRW@users.noreply.github.com>
2025-10-28 10:28:16 +0100

    Add files via upload

---
+++ repos/forSchleifeEinfacheUebung2/forSchleifeEinfacheUebung2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Deklaration
+int basis;
+int quadrat;
+int kubik;
+
+// Initialisierung
+basis = 0;
+quadrat = 1;
+kubik = 1;
+
+// Eingabe
+Console.WriteLine("\n\tProgramm");
+Console.WriteLine("\t========");
+
+// Verarbeitung
+
+
+// Ausgabe
+for (int i = 1; i <= 10; i++) {
+    basis = basis + 1;
+    quadrat = basis * basis;
+    kubik = quadrat * basis;
+    Console.WriteLine($"\n\tBasis = {basis}\tQuadrat = {quadrat}\tKubik = {kubik}");
+}
